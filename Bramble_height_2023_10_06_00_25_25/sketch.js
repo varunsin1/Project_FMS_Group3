4fe715c0ebe0791e973bd5dd5960b89d4a674ab0
Kevin81405 <147112783+Kevin81405@users.noreply.github.com>
2023-10-05 17:28:54 -0700

    Add files via upload

---
+++ Bramble_height_2023_10_06_00_25_25/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(700, 700);
+  background(220);
+  fill('purple')
+  square(350,450,200)
+  fill('blue')
+  square(150,450,200)
+  fill('red')
+  square(350,250,200)
+  fill('green')
+  square(150,250,200)
+}
+
+function draw() {
+  textSize(50)
+  fill('black')
+  text("Color Match", 210, 100)
+}
